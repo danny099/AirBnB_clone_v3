f031f768259376176bddb0783feba87d16b6a8e3
danny099 <dalmariv01@gmail.com>
2020-04-29 19:35:41 +0000

    error

--- api/v1/app.py
+++ api/v1/app.py
@@ -3,7 +3,7 @@
 from os import getenv
 from models import storage
 from api.v1.views import app_views
-from flask import Flask, Blueprint
+from flask import Flask, Blueprint, make_response, jsonify
 
 
 app = Flask(__name__)
@@ -13,6 +13,10 @@ app.register_blueprint(app_views)
 @app.teardown_appcontext
 def teardown(exception):
     storage.close()
+
+@app.errorhandler(404)
+def error_404(error):
+    return make_response(jsonify(error='Not found'), 404)
 
 if __name__ == "__main__":
     app.run(host=getenv('HBNB_API_HOST'), port=getenv('HBNB_API_PORT'),
