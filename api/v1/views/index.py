1b83d3e40b12c777810367a1201758626f4270de
danny099 <dalmariv01@gmail.com>
2020-04-29 19:20:21 +0000

    count stats

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -7,3 +7,12 @@ from api.v1.views import app_views
 @app_views.route("/status", strict_slashes=False)
 def status():
     return jsonify(status='OK')
+
+@app_views.route('/stats', strict_slashes=False)
+def stats():
+    return jsonify({'amenities': storage.count('Amenity'),
+                    'cities': storage.count('City'),
+                    'places': storage.count('Place'),
+                    'reviews': storage.count('Review'),
+                    'states': storage.count('State'),
+                    'users': storage.count('User')})
